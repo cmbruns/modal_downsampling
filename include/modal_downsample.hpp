9b464c8887be2f9ebbe6bd9a2decaea01dfa6cf3
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-01-14 18:26:22 -0500

    Move future idea out of header and into modal_downsample.cpp

--- include/modal_downsample.hpp
+++ include/modal_downsample.hpp
@@ -8,9 +8,9 @@
 namespace cmb {
 
 	/*
-	 * ModalDownsampler class downsamples a multidimensional raster image mipmap-style, 
-	   replacing each sub-block with the modal (most-frequent) value
-	   in the sub-block.
+	 * ModalDownsampler class recursively downsamples a multidimensional 
+	   raster image by one power of two, aggregating each sub-block into 
+	   the modal (most-frequent) value in the sub-block.
 
 	  Template parameters:
 	   LABEL_TYPE: must be the element type of the input raster image, 
@@ -22,41 +22,18 @@ namespace cmb {
 		 be the smallest integer type capable of holding such a value
 	 */
 	template<
-		typename LABEL_TYPE=uint32_t,
-		int DIMENSION_COUNT=3, 
-		typename LABEL_COUNT_TYPE=std::size_t>
+		typename LABEL_TYPE = uint32_t,
+		int DIMENSION_COUNT = 3, 
+		typename LABEL_COUNT_TYPE = std::size_t>
 	class ModalDownsampler {
 	public:
 		typedef LABEL_TYPE label_t;
 		typedef boost::multi_array<label_t, DIMENSION_COUNT> raster_t;
 		typedef std::vector<raster_t> downsamplings_t;
 		typedef LABEL_COUNT_TYPE label_count_t;
-		typedef boost::array<std::size_t, DIMENSION_COUNT> dimensions_t;
-		typedef std::vector<std::ostream*> output_streams_t;
 
 		// Top-level downsampler
 		downsamplings_t downsample(const raster_t& original);
-
-		// Below is the API for a future binary streaming interface, because it:
-		// 1) allows small memory implementations. For after everyone notices
-		//    this tool is used on an embarrassingly parallelizable pile of
-		//    blocks, and is better run as a large number of processes on one
-		//    multi-CPU cluster node, rather than as one huge memory-hog process 
-		//    that uses all the RAM on each node, and attempts to compensate 
-		//    with some way-too-clever multithreaded implementation. OK I don't
-		//    have a specific use case in mind for downsampling a terabyte image
-		//    on my iphone; but I like knowing it's possible.
-		// 2) allows streaming implementations. For after everyone notices that 
-		//    I/O is slower than the actual computation for tasks
-		//    like this. This will allow the computation to complete, and for
-		//    the output to finish writing, at almost the same moment that the 
-		//    original data block completes loading.
-		/*
-		void downsample(
-			dimensions_t dimensions,
-			std::istream& original, 
-			output_streams_t& outputs);
-			*/
 	};
 
 } // namespace cmb
--- src/modal_downsample.cpp
+++ src/modal_downsample.cpp
@@ -64,6 +64,55 @@ private:
 #endif
 };
 
+// Typedefs (or similar)
+// thank you C++11! for this 'using' thing
+
+// in case we want to experiment with other histogram types...
+template<typename LABEL_TYPE, typename LABEL_COUNT_TYPE>
+using histogram_t = map_histogram<LABEL_TYPE, LABEL_COUNT_TYPE>;
+
+// A shard is a contiguous one-dimensional run in the X-dimension
+template<typename LABEL_TYPE, typename LABEL_COUNT_TYPE>
+using shard_t = boost::multi_array<histogram_t<LABEL_TYPE, LABEL_COUNT_TYPE>, 1>;
+
+
+///// BEGIN POSSIBLE PARALLEL WORK UNIT METHODS //////
+
+// 1) Convert two consecutive original image label line sequences into a 
+//    downsampled histogram sequence.
+// TODO: maybe "original" argument should be a view...
+template<typename LABEL_TYPE, typename LABEL_COUNT_TYPE>
+shard_t<LABEL_TYPE, LABEL_COUNT_TYPE>
+aggregate_shard(const boost::multi_array<LABEL_TYPE, 2> & original)
+{
+	const int line_length = original.shape()[1];
+	const int scan_line_count = original.shape()[0];
+	// Assume two scan lines...
+	// (maybe one line would be OK here, in case of 1D downsamping?)
+	assert(scan_line_count == 2);
+
+	shard_t<LABEL_TYPE, LABEL_COUNT_TYPE> result_shard(line_length / 2);
+
+	// Two things happen here:
+	//   1) We downsample the labels in the X and Y directions (4X total)
+	//   2) We stop using raw labels, and begin using histograms
+	for (int scan_line = 0; scan_line < original.shape()[0]; ++scan_line) 
+	{
+		for (int i = 0; i < line_length; ++i) 
+		{
+			LABEL_TYPE label = original[scan_line][i];
+			histogram_t<LABEL_TYPE, LABEL_COUNT_TYPE> & pixel = result_shard[i / 2];
+			pixel.increment_label(label);
+		}
+	}
+	return result_shard;
+}
+
+// 2) Combine histograms from two shards
+
+///// END POSSIBLE PARALLEL WORK UNIT METHODS //////
+
+
 template<typename LABEL_TYPE, int DIMENSION_COUNT, typename LABEL_COUNT_TYPE>
 auto cmb::ModalDownsampler<LABEL_TYPE, DIMENSION_COUNT, LABEL_COUNT_TYPE>::downsample(const raster_t & original)
   -> downsamplings_t // trailing return type for better readability
@@ -87,10 +136,15 @@ auto cmb::ModalDownsampler<LABEL_TYPE, DIMENSION_COUNT, LABEL_COUNT_TYPE>::downs
 		histogram_t & pixel = shard[i / 2];
 		pixel.increment_label(label);
 	}
+
+	// Print sanity checks
 	for (histogram_t pixel : shard) {
 		for (auto count : pixel.getMap()) {
 			std::cout << (int)count.first << ", " << (int)count.second << std::endl;
 		}
+	}
+	for (int i = 0; i < DIMENSION_COUNT; ++i) {
+		std::cout << "stride: " << i+1 << ": " << original.strides()[i] << std::endl;
 	}
 
 	// TODO: Remove this hard-coded hack to return one particular answer
@@ -112,3 +166,25 @@ auto cmb::ModalDownsampler<LABEL_TYPE, DIMENSION_COUNT, LABEL_COUNT_TYPE>::downs
 template cmb::ModalDownsampler<uint8_t, 2, uint8_t>;
 
 // TODO: Put all this in the header, so the instantiation could happen automatically
+
+
+// Below is the API for a future binary streaming interface, because it:
+// 1) allows small memory implementations. For after everyone notices
+//    this tool is used on an embarrassingly parallelizable pile of
+//    blocks, and is better run as a large number of processes on one
+//    multi-CPU cluster node, rather than as one huge memory-hog process 
+//    that uses all the RAM on each node, and attempts to compensate 
+//    with some way-too-clever multithreaded implementation. OK I don't
+//    have a specific use case in mind for downsampling a terabyte image
+//    on my iphone; but I like knowing it's possible.
+// 2) allows streaming implementations. For after everyone notices that 
+//    I/O is slower than the actual computation for tasks
+//    like this. This will allow the computation to complete, and for
+//    the output to finish writing, at almost the same moment that the 
+//    original data block completes loading.
+/*
+void downsample(
+dimensions_t dimensions,
+std::istream& original,
+output_streams_t& outputs);
+*/
